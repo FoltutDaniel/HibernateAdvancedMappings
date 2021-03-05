df90ddb738a5b0903c0992b4984e7256f4a92aa9
Foltut Daniel <danfoltut@gmail.com>
2021-03-05 20:45:05 +0200

    OneToMany JDBC test ok`

---
+++ OneToMany/src/main/java/com/zeke/OneToMany/Test.java
@@ -0,0 +1,21 @@
+package com.zeke.OneToMany;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Test {
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to "+ jdbcUrl);
+			Connection myCon = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println(myCon);
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+}
